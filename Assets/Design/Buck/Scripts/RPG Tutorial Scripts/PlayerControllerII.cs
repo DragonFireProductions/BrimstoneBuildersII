79cf262deb7fc42efcea6e0125528d7aa7f293c2
Michael Buck II <MichaelBuck96@yahoo.com>
2018-11-17 02:00:53 -0500

    removed unnecessary scenes, started addinga  few basic scripts
    Quickly added my own movement and camera scripts to test some things, these aren't perminant and are just throw aways

---
+++ Assets/Design/Buck/Scripts/RPG Tutorial Scripts/PlayerControllerII.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotorII))]
+public class PlayerControllerII : MonoBehaviour
+{
+    //The layer that the player can move on
+    public LayerMask movementMask;
+
+    Camera cam;
+
+    PlayerMotorII motor;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotorII>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if right mouse button is pushed
+        if (Input.GetMouseButtonDown(1)) //Come back and swap out for input instead of mousebutton
+        {
+            //Draws a ray from the camera to where the mouse is
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+
+        //if left mouse button is pushed
+        if (Input.GetMouseButtonDown(0)) //Come back and swap out for input instead of mousebutton
+        {
+            //Draws a ray from the camera to where the mouse is
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                //check for interactable
+            }
+        }
+    }
+}
